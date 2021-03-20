78c204a3b0568d82b03a00698990abbd6bdb2e6e
Felipe Inostroza <felipeinostroza@NI-NTB-083.local>
2021-03-20 20:29:50 -0300

    Creating a personal Component and the first use of the props to add ours Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list';
 import './App.css';
+import './components/card-list/card-list.styles.css';
 
 
 class App extends Component{
@@ -23,9 +25,12 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        {
-          this.state.monsters.map( monster => <h1 key= { monster.id }>{ monster.name }</h1> )
-        }
+        <CardList>
+          {
+            this.state.monsters.map( monster => <h1 key= { monster.id }>{ monster.name }</h1> )
+          }
+        </CardList>
+        
       </div>
     );
   }
---
+++ src/components/card-list/card-list.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const CardList = (props) => {
+   return <div className='card-list'>{ props.children }</div>
+}  
