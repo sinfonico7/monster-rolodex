a609c8dce435a70974e925375fc9cc319d5c5e85
Felipe Inostroza <felipeinostroza@NI-NTB-083.local>
2021-03-22 02:52:16 -0300

    Updates

--- src/components/search-box/search-box.component.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './search-box.styles.css';
-
-export const SearchBox = ({placeholder,handlerChange}) => (
-    <input className='search' type='search' placeholder={placeholder} onChange={ handlerChange }/>
-);
